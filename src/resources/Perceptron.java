cc18c4eefe1c4f80435a0e7cd39710cbb91c28a0
Jaryn Seidl <jarynseidl@gmail.com>
2016-01-12 12:56:42 -0700

    Working on perceptron Train method.

--- src/resources/Perceptron.java
+++ src/resources/Perceptron.java
@@ -2,10 +2,35 @@ package resources;
 
 public class Perceptron extends SupervisedLearner {
 
+	double [] weights;
 	@Override
 	public void train(Matrix features, Matrix labels) throws Exception {
 		// TODO Auto-generated method stub
-		
+		weights = new double[features.cols()+1];
+		initializeWeights(weights);
+		int errors = 0;
+		int correct = 0;
+		double[] outputs = new double[features.rows()];
+		for(int i = 0;i<features.rows();i++){
+			int sum = 0;
+			for(int j = 0;j<features.row(i).length; j++){
+				sum+=features.get(i, j) * weights[j];
+			}
+			sum+=weights[weights.length-1];
+			if(sum>=0){
+				outputs[i] = 1;
+			}else{
+				outputs[i] = 0;
+			}
+			if(outputs[i]!=labels.get(i, 0)){
+				adjustWeights(features.row(i),weights,labels.get(i, 0), outputs[i]);
+				errors++;
+			}else{
+				correct++;
+			}
+		}
+		double classification_accuracy = correct/(correct + errors);
+		double classification_error = 1-classification_accuracy;
 	}
 
 	@Override
@@ -14,4 +39,15 @@ public class Perceptron extends SupervisedLearner {
 
 	}
 
+	private void initializeWeights(double[] weights){
+		for(double weight: weights){
+			weight = Math.random();
+		}
+	}
+	private void adjustWeights(double[] values, double[] weights, double target, double output){
+		double learningRate = .1;
+		for(int i = 0;i<weights.length;i++){
+			weights[i] += learningRate * (target - output) * values[i];
+		}
+	}
 }

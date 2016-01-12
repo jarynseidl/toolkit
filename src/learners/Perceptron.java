5e969004401af226ec4490e0691f25fef27176cf
Jaryn Seidl <jarynseidl@gmail.com>
2016-01-12 12:04:00 -0700

    First commit

---
+++ src/learners/Perceptron.java
@@ -0,0 +1,20 @@
+package learners;
+
+import resources.Matrix;
+import resources.SupervisedLearner;
+
+public class Perceptron extends SupervisedLearner {
+
+	@Override
+	public void train(Matrix features, Matrix labels) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void predict(double[] features, double[] labels) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+}
